fa4c9474db898f3a264207a16b0c11e13337d50f
Erwin Heba <erwin.heba@twoupdigital.com>
2022-12-22 18:51:51 +0100

    exercise-01 - prompt(), if else

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,13 @@
 
 // 3. Ile subskrybcji przybywa miesięcznie
 // 4. Oblicz jaki status będzie miał w nastepne 12mc
+
+const subscribersNumber = prompt("How many subscribers do you have?");
+
+if (subscribersNumber >= 100 && subscribersNumber < 1000) {
+    console.log('You have bronze status')
+} else if (subscribersNumber >= 1000 && subscribersNumber < 10000) {
+    console.log('You have gold status')
+} else if (subscribersNumber >= 10000) {
+    console.log('You have diamond status')
+};

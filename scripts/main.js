89104dae7004fdff1478034ade360a49c91f7ea9
patrycjalesniak <64854205+patrycjalesniak@users.noreply.github.com>
2022-12-22 19:03:57 +0100

    exercise-01-sub in next Year

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,17 @@ if (subscribersNumber >= 100 && subscribersNumber < 1000) {
 } else if (subscribersNumber >= 10000) {
     console.log('You have diamond status')
 };
+
+const subPerMonth = prompt("How many subscribers do you have per month?");
+
+const subPerYear = subPerMonth * 12;
+
+const subInNextYear = subscribersNumber + subPerYear;
+
+if (subInNextYear >= 100 && subInNextYear < 1000) {
+    console.log('You have bronze status')
+} else if (subInNextYear >= 1000 && subInNextYear < 10000) {
+    console.log('You have gold status')
+} else if (subInNextYear >= 10000) {
+    console.log('You have diamond status')
+};

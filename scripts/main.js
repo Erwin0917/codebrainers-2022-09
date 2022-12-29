abcbb5ef5ce25c9f78787ffd47947a004214d958
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2022-12-29 18:42:27 +0100

    homework solution

--- scripts/main.js
+++ scripts/main.js
@@ -6,37 +6,60 @@
 // 5. Pytaj użytkownika tak dużo razy aż poda numer *
 // 6. Dodaj obsługę przycisku anuluj *
 
-const subscribersNumber = parseInt(prompt("How many subscribers do you have?"));
+let subscribersNumber;
+let isValueNumber;
 
-console.log(subscribersNumber)
-if (isNaN(subscribersNumber)) {
-    alert('value is not a number')
+console.log(subscribersNumber);
+
+document.write(subscribersNumber + "<br>");
+
+do {
+  subscribersNumber = prompt("How many subscribers do you have?");
+  isValueNumber = isNaN(parseInt(subscribersNumber));
+  if (isValueNumber && subscribersNumber !== null) {
+    alert("Value not a number");
+  }
+} while (isValueNumber && subscribersNumber !== null);
+
+if (subscribersNumber !== null) {
+  console.log(subscribersNumber);
+
+  document.write(subscribersNumber + "<br>");
+
+  if (subscribersNumber >= 100 && subscribersNumber < 1000) {
+    console.log("You have bronze status");
+    document.write("You have bronze status");
+  } else if (subscribersNumber >= 1000 && subscribersNumber < 10000) {
+    console.log("You have gold status");
+    document.write("You have gold status");
+  } else if (subscribersNumber >= 10000) {
+    console.log("You have diamond status");
+    document.write("You have diamond status");
+  }
+
+  let subPerMonth;
+
+  console.log(subPerMonth);
+  document.write("<br>" + subPerMonth + "<br>");
+
+  do {
+    subPerMonth = prompt("Hon many subscribers increase monthly?");
+    if (isNaN(parseInt(subPerMonth))) {
+      alert("Value not a number");
+    }
+  } while (isNaN(parseInt(subPerMonth)));
+
+  const subPerYear = subPerMonth * 12;
+  const subInNextYear = parseInt(subscribersNumber) + subPerYear;
+
+  if (subInNextYear >= 100 && subInNextYear < 1000) {
+    console.log("You will have bronze status");
+    document.write("You will have bronze status");
+  } else if (subInNextYear >= 1000 && subInNextYear < 10000) {
+    console.log("You will have gold status");
+    document.write("You will have gold status");
+  } else if (subInNextYear >= 10000) {
+    console.log("You will have diamond status");
+    document.write("You will have diamond status");
+  }
 }
-
-if (subscribersNumber >= 100 && subscribersNumber < 1000) {
-    console.log('You have bronze status')
-} else if (subscribersNumber >= 1000 && subscribersNumber < 10000) {
-    console.log('You have gold status')
-} else if (subscribersNumber >= 10000) {
-    console.log('You have diamond status')
-};
-
-const subPerMonth = parseInt(prompt("How many subscribers do you have per month?"));
-
-console.log(subPerMonth)
-if (isNaN(subPerMonth)) {
-    alert('value is not a number')
-}
-
-const subPerYear = subPerMonth * 12;
-
-const subInNextYear = subscribersNumber + subPerYear;
-
-if (subInNextYear >= 100 && subInNextYear < 1000) {
-    console.log('You have bronze status')
-} else if (subInNextYear >= 1000 && subInNextYear < 10000) {
-    console.log('You have gold status')
-} else if (subInNextYear >= 10000) {
-    console.log('You have diamond status')
-};
-

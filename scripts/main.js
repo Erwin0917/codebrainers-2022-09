4c97076b0377f62ea464c66a9df3680684592f0b
Ewelina Kociolek <ewelina.kociolek@wp.pl>
2023-01-02 19:09:09 +0100

    ek-exercise-02-homework-solution

--- scripts/main.js
+++ scripts/main.js
@@ -15,16 +15,48 @@ function between(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+let trueAnswer = 0;
+let falseAnswer = 0;
+console.log(trueAnswer);
+console.log(falseAnswer);
+
 // const myNumber = between(0, 10);
+for (let num = 1; num <= 10; num ++) {
 
-const firstNumber = between(0, 10);
-const secondNumber = between(0, 10);
-const multResult = prompt(
-  `What's the result? ${firstNumber} * ${secondNumber}`
-);
+  const firstNumber = between(0, 10);
+  const secondNumber = between(0, 10);
 
-if (firstNumber * secondNumber === parseInt(multResult)) {
-  alert("Correct result");
+  do {
+    multResult = prompt(`What's the result? ${firstNumber} * ${secondNumber}`);
+    isResultNumber = isNaN(parseInt(multResult));
+    if (isResultNumber && multResult !== null) {
+      alert("Walue not a number!");
+    }
+  } while (isResultNumber && multResult !== null);
+
+  if (firstNumber * secondNumber === parseInt(multResult)) {
+    alert("Correct result");
+    console.log("Prawidłowa odpowiedź")
+    trueAnswer = trueAnswer + 1;
+  } else {
+    alert("Incorrect result");
+    console.log("Nieprawidłowa odpowiedź")
+    falseAnswer = falseAnswer + 1;
+  }
+}
+
+console.log(trueAnswer);
+console.log(falseAnswer);
+
+
+if (trueAnswer >= 5) {
+  alert("Test passed");
+  console.log("Test zaliczony");
+  document.write("Prawidłowe odpowiedzi: " + trueAnswer + "/10" + "</br>" );
+  document.write("Gratulacje! Test zaliczony.")
 } else {
-  alert("Incorrect result");
+  alert("Test failed");
+  console.log("Test niezaliczony");
+  document.write("Prawidłowe odpowiedzi: " + trueAnswer + "/10" + "</br>" );
+  document.write("Test niezaliczony.");
 }

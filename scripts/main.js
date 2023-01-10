15d1b19415495483b1290c696c2be59230873098
Erwin Heba <erwin.heba@playbookengineering.com>
2023-01-10 20:18:05 +0100

    exercise-03 - lotto init

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,7 @@
-const arr = [];
-const nonEmptyArr = ['kasia', 'jan', 'zosia', [10, 50]];
-const arrExample = new Array(5);
-console.log(' arrExample', arrExample);
+// ---- sprawdzenie ile potrzeba losowań lotto żeby trafić 6 -----
 
 
 
-
-// push() / pop()
-// unshift() / shift()
-
-console.log('nonEmptyArr first elem ', nonEmptyArr);
-nonEmptyArr.push('adam');
-console.log('nonEmptyArr first elem ', nonEmptyArr);
-const lastElem = nonEmptyArr.pop();
-console.log('nonEmptyArr first elem ', nonEmptyArr);
-console.log('lastElem ', lastElem);
-console.log('nonEmptyArr 2', nonEmptyArr[3][1]);
-
-
-// indexOf() - index elementu lub -1 jeśli nie ma szukanego elementu
-
-console.log('nonEmptyArr indexOf ', nonEmptyArr.indexOf('adam'));
+function between(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}

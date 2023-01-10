d518f5e305ad460686342172992534e1e8a30f99
Erwin Heba <erwin.heba@playbookengineering.com>
2023-01-10 19:57:28 +0100

    exercise-03 - Array, pop(), push(), shift(), unshift(), indexOf()

--- scripts/main.js
+++ scripts/main.js
@@ -1,72 +1,23 @@
-//1. Losowanie liczb 0-10
-//2. Zapytanie usera o wynik mnożenia dwóch wylosowanych wcześniej liczb
+const arr = [];
+const nonEmptyArr = ['kasia', 'jan', 'zosia', [10, 50]];
+const arrExample = new Array(5);
+console.log(' arrExample', arrExample);
 
-//3. 10 pytań z mnożenia
-//4. Wyświetlenie wyniku testu - 50%+ test zdany
 
-// Math.random(); 0 - 0.9(999);
-//
-// Math.round(1.5) => 2
-//
-// Math.floor()
-// Math.ceil()
 
-// liczba pytań zależna od użytkownika
-// licza pytań jako zmienna
 
-function between(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// push() / pop()
+// unshift() / shift()
 
-function isValueNumber(value) {
-  return isNaN(parseInt(value));
-}
+console.log('nonEmptyArr first elem ', nonEmptyArr);
+nonEmptyArr.push('adam');
+console.log('nonEmptyArr first elem ', nonEmptyArr);
+const lastElem = nonEmptyArr.pop();
+console.log('nonEmptyArr first elem ', nonEmptyArr);
+console.log('lastElem ', lastElem);
+console.log('nonEmptyArr 2', nonEmptyArr[3][1]);
 
-let correctAnswer = 0;
-let incorrectAnswer = 0;
-// let isValueNumber;
-let numberOfQuestions;
-const percentPassed = 0.5;
 
-numberOfQuestions = prompt("How many questions?");
+// indexOf() - index elementu lub -1 jeśli nie ma szukanego elementu
 
-if (!isValueNumber(numberOfQuestions)) {
-  process.exit();
-}
-
-for (let i = 0; i < numberOfQuestions; i++) {
-  const firstNumber = between(0, 10);
-  const secondNumber = between(0, 10);
-
-  do {
-    multResult = prompt(`What's the result? ${firstNumber} * ${secondNumber}`);
-    // isValueNumber = isNaN(parseInt(multResult));
-
-    if (multResult === null) {
-      alert("Thank you!");
-      process.exit();
-    }
-
-    if (isValueNumber(multResult) && multResult !== null) {
-      alert("Value not a number");
-    } else if (firstNumber * secondNumber === parseInt(multResult)) {
-      {
-        correctAnswer = parseInt(correctAnswer) + 1;
-      }
-    } else {
-      incorrectAnswer = parseInt(incorrectAnswer) + 1;
-    }
-  } while (isValueNumber(multResult) && multResult !== null);
-}
-
-if (correctAnswer / numberOfQuestions >= percentPassed) {
-  alert(`Test passed! ${correctAnswer} / ${numberOfQuestions}`);
-} else {
-  alert(
-    `Test failed, score below 50%: ${correctAnswer} / ${numberOfQuestions}`
-  );
-}
-
-console.log(correctAnswer);
-console.log(incorrectAnswer);
-console.log(`${(correctAnswer / numberOfQuestions) * 100}%`);
+console.log('nonEmptyArr indexOf ', nonEmptyArr.indexOf('adam'));

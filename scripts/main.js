18853f3d518430a00fb92a21ce1a7533286c2e4d
Joanna Chorazy <aska.chorazy@gmail.com>
2023-01-10 20:48:05 +0100

    exercise-03 availableNumbers

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,13 @@
 function between(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+
+const availableNumbers = [];
+
+for (let i = 1; i <= 49; i++){ 
+  availableNumbers.push(i) 
+
+}
+
+console.log(availableNumbers)

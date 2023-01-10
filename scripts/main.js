5630729dc13a2bab2d0915c912b013108f6700a4
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-01-10 18:20:25 +0100

    exercise-02 homework solution

--- scripts/main.js
+++ scripts/main.js
@@ -11,20 +11,53 @@
 // Math.floor()
 // Math.ceil()
 
+// liczba pytań zależna od użytkownika
+// licza pytań jako zmienna
+
 function between(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// const myNumber = between(0, 10);
+let correctAnswer = 0;
+let incorrectAnswer = 0;
+let isValueNumber;
+// const percentPassed = 50%;
 
-const firstNumber = between(0, 10);
-const secondNumber = between(0, 10);
-const multResult = prompt(
-  `What's the result? ${firstNumber} * ${secondNumber}`
-);
+for (let i = 0; i < 10; i++) {
+  const firstNumber = between(0, 10);
+  const secondNumber = between(0, 10);
 
-if (firstNumber * secondNumber === parseInt(multResult)) {
-  alert("Correct result");
-} else {
-  alert("Incorrect result");
+  do {
+    multResult = prompt(`What's the result? ${firstNumber} * ${secondNumber}`);
+    isValueNumber = isNaN(parseInt(multResult));
+
+    if (multResult === null) {
+      alert("Thank you!");
+      process.exit();
+    }
+
+    if (isValueNumber && multResult !== null) {
+      alert("Value not a number");
+    } else if (firstNumber * secondNumber === parseInt(multResult)) {
+      {
+        correctAnswer = parseInt(correctAnswer) + 1;
+      }
+    } else {
+      incorrectAnswer = parseInt(incorrectAnswer) + 1;
+    }
+  } while (isValueNumber && multResult !== null);
 }
+
+if (correctAnswer >= 5) {
+  alert(`Test passed! ${correctAnswer} / ${correctAnswer + incorrectAnswer}`);
+} else {
+  alert(
+    `Test failed, score below 50%: ${correctAnswer} / ${
+      correctAnswer + incorrectAnswer
+    }`
+  );
+}
+
+console.log(correctAnswer);
+console.log(incorrectAnswer);
+console.log(`${(correctAnswer / (correctAnswer + incorrectAnswer)) * 100}%`);

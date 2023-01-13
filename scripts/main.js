707073704bf50e18d6b8e944eebb21bc3076ab9d
Ewelina Kociolek <ewelina.kociolek@wp.pl>
2023-01-13 22:47:05 +0100

    ek-exercise-03 random numbers

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,22 @@ function between(min, max) {
 
 const availableNumbers = [];
 
-for (let i = 1; i <= 49; i++){ 
-  availableNumbers.push(i) 
+for (let i = 1; i <= 49; i++){
+  availableNumbers.push(i)
 
 }
 
 console.log(availableNumbers)
+
+
+
+const randomNumbers = [];
+
+for (let i = 0; i < 6; i++){
+  randomNumbers.push(availableNumbers[between(0, availableNumbers.length - 1)])
+}
+
+console.log(randomNumbers)
+
+const sixNumbers = [12, 43, 29, 31, 1, 8];
+

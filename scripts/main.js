ea9a2c37ec47cfcd71054ce47ae49cae519c513b
Ewelina Kociolek <ewelina.kociolek@wp.pl>
2023-01-14 00:07:55 +0100

    ek-exercise-03 homework

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ function between(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// drawing 49 numbers
 
 const availableNumbers = [];
 
@@ -18,13 +19,17 @@ console.log(availableNumbers)
 
 
 
+// drawing six random numbers
+
 const randomNumbers = [];
 
 for (let i = 0; i < 6; i++){
-  randomNumbers.push(availableNumbers[between(0, availableNumbers.length - 1)])
+  randomNumbers.push(availableNumbers.splice(Math.floor(Math.random() * availableNumbers.length), 1)[0])
 }
 
 console.log(randomNumbers)
 
-const sixNumbers = [12, 43, 29, 31, 1, 8];
+
+const sixNumbers = [6, 15, 22, 41, 48, 30];
+
 

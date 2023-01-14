32b1ba224fb06bd8b2e14c080fd0833c85d3bf4b
DominikHebda <hebdad@wp.pl>
2023-01-14 10:57:05 +0100

    exercise-03 lotto-drawingNumbers

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,38 @@ function between(min, max) {
 
 const availableNumbers = [];
 
-for (let i = 1; i <= 49; i++){ 
-  availableNumbers.push(i) 
+for (let i = 1; i <= 49; i++) {
+  availableNumbers.push(i)
 
 }
 
 console.log(availableNumbers)
+
+function getNumbersDrawn() {
+  const numbersDrawn = []
+  for (let i = 0; i < 6; i++) {
+    const ballIndex = Math.floor(Math.random() * availableNumbers.length);
+    const removedNumbers = availableNumbers.splice(ballIndex, 1);
+    const ballNumber = removedNumbers[0];
+    numbersDrawn.push(ballNumber);
+  }
+  return numbersDrawn
+}
+
+function compareArraysNumbers(array1, array2) {
+
+}
+
+function drawingsNumbers() {
+  const numberSelected = [2, 5, 15, 40, 41, 49];
+  let jackpot = false;
+  let ticketsCount = 0;
+  while (jackpot === false) {
+    const numbersDrawn = getNumbersDrawn();
+    jackpot = compareArraysNumbers(numberSelected, numbersDrawn);
+    ticketsCount = ticketsCount + 1;
+    // ticketsCount++ tożsame z powyższym
+  }
+  return ticketsCount
+
+}

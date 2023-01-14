c1d5f1f525fab79bd43bb8596fcbceb9b0ecc5f8
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-01-14 15:43:23 +0100

    exercise-04 callback function

--- scripts/main.js
+++ scripts/main.js
@@ -7,28 +7,24 @@
 // console.log('a ', a);
 // console.log('b ', b);
 
-
 const exampleArray = [
-  'Emilia',
-  'Szymon',
-  'Adrian',
-  'Ola',
-  'Franek',
+  "Emilia",
+  "Szymon",
+  "Adrian",
+  "Ola",
+  "Franek",
   1,
   33,
   12,
   undefined,
   null,
-  [
-    'Warszawa', 'Kraków', 'Wrocław'
-  ],
+  ["Warszawa", "Kraków", "Wrocław"],
   {
-    name: 'Opel',
-    color: 'red'
+    name: "Opel",
+    color: "red",
   },
-  NaN
+  NaN,
 ];
-
 
 // function square(x){
 //     return x * x
@@ -38,21 +34,20 @@ const exampleArray = [
 //   return x * x;
 // }
 
-const square = x => x * x;
-
+const square = (x) => x * x;
 
 function squareIfNumber(x) {
-  return typeof x === 'number' ? x * x : x;
+  return typeof x === "number" ? x * x : x;
 }
 
 const fullArrayWithSquared = exampleArray.map(squareIfNumber);
-console.log('fullArrayWithSquared ', fullArrayWithSquared);
+console.log("fullArrayWithSquared ", fullArrayWithSquared);
 
 // const nextArray = exampleArray.map(square);
 // exampleArray.forEach(square)
 
 function filterOutNonString(value) {
-  return typeof value === 'string';
+  return typeof value === "string";
 
   // if (typeof value === 'string') {
   //     return true;
@@ -60,39 +55,51 @@ function filterOutNonString(value) {
   // return false;
 }
 
-
-const nextArray = exampleArray.filter(x => filterOutNonString(x)).reverse()
-
+const nextArray = exampleArray.filter((x) => filterOutNonString(x)).reverse();
 
 // console.log('join',exampleArray.join('--'))
 // nextArray.reverse();
 
-
 // console.log('nextArray ', nextArray);
 
-
 const numbers = exampleArray.filter(function (x) {
-  if (typeof x === 'number' && isNaN(x) === false) {
+  if (typeof x === "number" && isNaN(x) === false) {
     return true;
-
   }
-})
+});
 
 const squaredNumbers = numbers.map(square);
 // console.log(squaredNumbers)
 
-const letterLongThree = exampleArray.filter(function (y) {
-
-  if (filterOutNonString(y) && y.length > 3) {
-    return true
-  }
-
-}
-).reverse()
+const letterLongThree = exampleArray
+  .filter(function (y) {
+    if (filterOutNonString(y) && y.length > 3) {
+      return true;
+    }
+  })
+  .reverse();
 
 const filterOutObjects = (element) => {
-  return typeof element === 'object' && element !== null && !Array.isArray(element) ? true : false
+  return typeof element === "object" &&
+    element !== null &&
+    !Array.isArray(element)
+    ? true
+    : false;
+};
+const arrayOfObjects = exampleArray.filter(filterOutObjects);
+console.log("selement?", arrayOfObjects);
 
+function filterArray(array, filterCallback) {
+  const filteredArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (filterCallback(element)) {
+      filteredArray.push(element);
+    }
+  }
+
+  return filteredArray;
 }
-const arrayOfObjects = exampleArray.filter(filterOutObjects)
-console.log('selement?', arrayOfObjects)
+
+const arrayOfObjects2 = filterArray(exampleArray, filterOutObjects);
+console.log("arrayOfObjects2", arrayOfObjects2);

9e7b807282bc6359fe67daec49e92b5f63ba55aa
Erwin Heba <erwin.heba@playbookengineering.com>
2023-01-14 13:53:20 +0100

    exercise-04 - Array methods - map, forEach, filter...

--- scripts/main.js
+++ scripts/main.js
@@ -1,61 +1,52 @@
-// ---- sprawdzenie ile potrzeba losowań lotto żeby trafić 6 -----
+// const a = [1, 2, 3];
+// // const b = [1, 2, 3];
+// const b = a;
+// console.log(JSON.parse('{}'))
+// b[0] = 13;
+//
+// console.log('a ', a);
+// console.log('b ', b);
 
-function between(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+
+const exampleArray = [
+    'Emilia',
+    'Szymon',
+    'Adrian',
+    'Ola',
+    'Franek',
+    1,
+    33,
+    12,
+    undefined,
+    null,
+    [
+        'Warszawa', 'Kraków', 'Wrocław'
+    ],
+    {
+      name: 'Opel',
+      color: 'red'
+    },
+    NaN
+];
+
+function square(x) {
+  return x * 2;
 }
 
-function getNumbers() {
-  const availableNumbers = [];
-  for (let i = 1; i <= 40; i++) {
-    availableNumbers.push(i);
+// const nextArray = exampleArray.map(square);
+// exampleArray.forEach(square)
+
+
+const nextArray = exampleArray.filter(function (x){
+  if (typeof x === 'string') {
+    return true;
   }
-  return availableNumbers;
-}
+  return false;
+})
 
-function getNumbersDrawn() {
-  const numbersDrawn = [];
-  const availableNumbers = getNumbers();
-  for (let i = 0; i < 6; i++) {
-    const ballIndex = Math.floor(Math.random() * availableNumbers.length);
-    const removedNumbers = availableNumbers.splice(ballIndex, 1);
-    const ballNumber = removedNumbers[0];
-    numbersDrawn.push(ballNumber);
-  }
 
-  return numbersDrawn;
-}
+// console.log('join',exampleArray.join('--'))
+// nextArray.reverse();
 
-function compareArraysNumbers(array1, array2) {
-  let index = 0;
-  if (array1.length !== array2.length) {
-    return false;
-  }
 
-  do {
-    if (array1[index] !== array2[index]) {
-      return false;
-    }
-    index++;
-  } while (index < array1.length);
-  return true;
-}
-
-// console.log("should be true",compareArraysNumbers([2,4,6],[2,4,6]))
-// console.log("should be false",compareArraysNumbers([2,4,6],[2,6,10]))
-function comperator(a, b) {
-  return a - b;
-}
-
-function drawingsNumbers() {
-  const numberSelected = [2, 5, 3, 7, 1, 9].sort(comperator);
-  let jackpot = false;
-  let ticketsCount = 0;
-  while (jackpot === false) {
-    const numbersDrawn = getNumbersDrawn().sort(comperator);
-    jackpot = compareArraysNumbers(numberSelected, numbersDrawn);
-    ticketsCount = ticketsCount + 1;
-    // ticketsCount++ tożsame z powyższym
-  }
-  return ticketsCount;
-}
-console.log(drawingsNumbers());
+console.log('nextArray ', nextArray);

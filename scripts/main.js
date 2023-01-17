512a1d19d757c23881c1214e56391189fc4f2874
Tomasz Pinter <tomaszpinter@gmail.com>
2023-01-17 19:48:52 +0100

    exercise-05  number of gears and maxSpeed simpleValidation

--- scripts/main.js
+++ scripts/main.js
@@ -12,12 +12,26 @@ simpleObj.printName()
 class Car {
   constructor(color = 'blue', maxSpeed, numberOfGears) {
     this.color = color;
-    if (maxSpeed !== undefined && numberOfGears !== undefined && typeof maxSpeed === 'number' && typeof numberOfGears === 'number'){
-      this.maxSpeed = maxSpeed;
-      this.numberOfGears = numberOfGears;
-    }
+    // if (maxSpeed !== undefined && numberOfGears !== undefined && typeof maxSpeed === 'number' && typeof numberOfGears === 'number'){
+    //   this.maxSpeed = maxSpeed;
+    //   this.numberOfGears = numberOfGears;
+    // }
     this.speed = 0;
+
+    if (numberOfGears !== undefined && numberOfGears !== 0 && typeof numberOfGears === 'number' ) {
+        this.numberOfGears = numberOfGears;
+    } else { 
+      this.numberOfGears = 6;
+    }
+    
+    if (maxSpeed !== undefined && maxSpeed !== 0 && typeof maxSpeed === 'number' ) {
+      this.maxSpeed = maxSpeed;
+    } else {
+      this.maxSpeed = 250;
+    }
+
   }
+    
 
   setPaint(color) {
     this.color = color;

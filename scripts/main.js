16591aaf7d2210454afe3d97f0fd8b7fdef74e67
Ewelina Kociolek <ewelina.kociolek@wp.pl>
2023-01-20 19:32:29 +0100

    ek-exercise-05 homework solution part 2

--- scripts/main.js
+++ scripts/main.js
@@ -11,128 +11,139 @@ simpleObj.printName()
 
 
 const isCorrectNumber = (value) => {
-    if (value !== undefined && value > 0 && typeof value === 'number'){ 
-        return true;
-    }
-    return false;
+  return typeof value === 'number' && value >= 0 && isNaN(value) === false;
 }
 
-const ifNumberIsString = (value) => {
-    if (value !== undefined && value > 0 && typeof value === 'string' && parseInt(value) !== null){
-        return true;
-    }
-    return false;
-}
-
-const isCorrectColor = (value) => {
-    if (value !== undefined && typeof value === 'string'){
-        return true;
-    }
-    return false;
+const setNumericValue = (value, defaultValue) => {
+  const parsedValue = parseInt(value);
+  if (isCorrectNumber(parsedValue) && parsedValue > 0) {
+    return parsedValue;
+  }
+  return defaultValue;
 }
 
 
+const isCorrectColor = color => typeof color !== "number" && color !== null;
 
 
 class Car {
-  constructor(color = 'blue', maxSpeed, numberOfGears) {
-    this.speed = 0;
-    this.color = color;
+  constructor(color = "blue", maxSpeed, numberOfGears) {
     if (isCorrectColor(color)) {
-        this.color = color;
+      this.color = color;
     } else {
-        this.color = 'blue';
+      throw new Error("Color must be a string")
     }
 
-    if (isCorrectNumber(numberOfGears)) {
-      this.numberOfGears = numberOfGears;
-    } else {
-      this.numberOfGears = 6;
-    }
-    if (ifNumberIsString(numberOfGears)){
-        this.numberOfGears = parseInt(numberOfGears);
-    }
-
-    if (isCorrectNumber(maxSpeed)) {
-      this.maxSpeed = maxSpeed;
-    } else {
-      this.maxSpeed = 250;
-    }
-    if (ifNumberIsString(maxSpeed)){
-        this.maxSpeed = parseInt(maxSpeed);
-    }
+    this.speed = 0;
+    this.maxSpeed = setNumericValue(maxSpeed, 250);
+    this.numberOfGears = setNumericValue(numberOfGears, 6);
 
   }
 
-
-  setPaint(colorNew){
-    if (isCorrectColor(colorNew))
-    this.color = colorNew;
+  getColor(color) {
+    console.log(`Current color of car: ${this.color}`);
   }
 
-  getColor() {
-    return this.color;
-    //console.log(car2.getColor())
+  setColor(color) {
+    if (isCorrectColor(color)) {
+      this.color = color;
+      console.log(`Color updated to ${this.color}`);
+    } else {
+      console.error(
+        `Color not updated, must be a string, you entered: ${color}`
+      );
+    }
+  }
+
+  getSpeed(speed) {
+    console.log(`Current speed of car: ${this.speed}`);
   }
 
   setSpeed(speed) {
-    if (isCorrectNumber(speed) || ifNumberIsString(speed)){
-        this.speed = parseInt(speed);
-        if (this.speed >= this.maxSpeed) this.speed = this.maxSpeed;
+    if (setNumericValue(speed) <= this.maxSpeed){
+        this.speed = parseInt(speed)
+        console.log(`New speek set to ${this.speed}`)
+    } else {
+        this.speed > this.maxSpeed
+        console.log(`Speed set to max ${this.maxSpeed}`)
+        }
     }
+
+  getNumberOfGears() {
+    console.log(`Current no of gears: ${this.numberOfGears}`);
   }
-  
 
+  setNumberOfGears(numberOfGears) {
+    if (setNumericValue(numberOfGears)){
+        this.numberOfGears = setNumericValue(numberOfGears)
+            console.log(`New number of gears ${this.numberOfGears}`)
+        } else {
+            console.log(`Number of gears stay same (${this.numberOfGears})`)
+        }
+  }
   accelerate(howMuch) {
-    if (isCorrectNumber(howMuch) || ifNumberIsString(howMuch)) {
-        this.speed = this.speed + parseInt(howMuch);
-        if (this.speed > this.maxSpeed) this.speed = this.maxSpeed;
+    if (this.speed + setNumericValue(howMuch) <= this.maxSpeed){
+        this.speed = this.speed + setNumericValue(howMuch);
+        console.log(`Accelerate to ${this.speed}`)
+    } else {
+        this.speed = this.maxSpeed;
+        console.log(`Accelerate to max speed ${this.maxSpeed}`)
     }
-
   }
 
   decelerate(howMuch) {
-    if (isCorrectNumber(howMuch) || ifNumberIsString(howMuch)) {
-        this.speed = this.speed - parseInt(howMuch);
-        if (this.speed < 0) this.speed = 0;
+    if (this.speed - setNumericValue(howMuch) >= 0){
+        this.speed = this.speed - setNumericValue(howMuch);
+        console.log(`Decelerate to ${this.speed}`)
+    } else {
+        this.speed = 0;
+        console.log(`Decelerate car stop`)
     }
-
   }
-
 }
 
-const car1 = new Car('red', 50, 4);
-const car2 = new Car('black', '150', '7');
-const car3 = new Car('red', 'fast', 'one');
-const car4 = new Car('red', -20, -2);
+const car1 = new Car("blue", "50", "4", false); //true
+const car2 = new Car("black", -150, "8", false); //true
+const car3 = new Car("red", 500, "one", true); //false
+const car4 = new Car("yellow", -20, -2, true); //false
+
+// car3.setSpeed(500);
+// car4.setSpeed(100);
+// car2.setAccelerate(250);
+// car3.setAccelerate("left");
+
 const cars = [car1, car2, car3, car4];
 
+// console.log("cars ", cars);
 
-console.log('before', cars);
+// console.log("car4: ", car4);
 
-car3.setSpeed(500);
-car4.setSpeed(null);
-car2.accelerate('250');
-car3.accelerate('left');
-car3.setPaint("orange");
-car1.accelerate(100);
+// car4.getColor();
+// car4.setColor("violet");
+// car4.getColor();
+car4.getSpeed();
+car4.setSpeed(100);
+car4.getSpeed();
 
-console.log('mods', cars);
+console.log('-'.repeat(20));
 
-car1.decelerate(150);
-car1.setPaint('pink');
-car3.accelerate('100');
+car4.getNumberOfGears();
+car4.setNumberOfGears(10);
+car4.getNumberOfGears();
 
-console.log('mods2', cars);
+console.log('-'.repeat(20));
+
+car4.getSpeed();
+car4.accelerate(155);
+car4.getSpeed();
+car4.decelerate(200);
+car4.getSpeed();
+
+console.log("car4: ", car4);
+console.log("car1: ", car1);
+car1.accelerate("70");
+console.log("car1: ", car1);
+car1.setNumberOfGears("3");
+console.log("car1: ", car1);
 
 
-
-
-
-
-
-
-// console.log('firstCar ', firstCar.color);
-
-// secondCar.setPaint('black');
-// console.log('secondCar ', secondCar);

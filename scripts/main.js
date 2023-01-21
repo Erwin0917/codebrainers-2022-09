da65372bc0b1dcf09e9082d51859eda6ea2eaf92
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-01-21 10:53:20 +0100

    exercise-06 attack(), duel function

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,16 @@
+function between(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 class Person {
   constructor(hitPoints) {
     this.hitPoints = hitPoints;
   }
-
-
-
 }
 
 class Character {
   constructor(hitPoints, strength) {
-    this.hitPoints = hitPoints
+    this.hitPoints = hitPoints;
     this.strength = strength;
   }
 
@@ -17,10 +18,14 @@ class Character {
     return this.hitPoints > 0;
   }
 
-  attack() {
-
+  attack(victim) {
+    if (victim instanceof Character) {
+      victim.hitPoints -= between(1, 100);
+      victim.strength -= between(1, 50);
+      return;
+    }
+    throw new Error(`(victim) not instance of Character`, victim);
   }
-
 }
 
 class Hero extends Character {
@@ -35,5 +40,16 @@ class Criminal extends Character {
   }
 }
 
+const hero = new Hero(100, 100);
+const criminal = new Criminal(90, 90);
 
-
+const duel = () => {
+  do {
+    hero.attack(criminal);
+    if (criminal.isAlive()) {
+      criminal.attack(hero);
+    }
+    console.log("criminal", criminal);
+    console.log("hero", hero);
+  } while (criminal.isAlive() && hero.isAlive());
+};

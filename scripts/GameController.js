2a8044f263cd55ff64cfaaf185dd4256ce1f204a
DymitrGafarowski <dymitr.gafarowski@interia.pl>
2023-01-26 20:59:18 +0100

    excercise-06 add to team

--- scripts/GameController.js
+++ scripts/GameController.js
@@ -13,7 +13,7 @@ export class GameController {
         }
 
         if (gameHtmlWrapper.querySelector('#gameControlWrapper')) {
-            this.controlPanel = new GameControlPanel(gameHtmlWrapper.querySelector('#gameControlWrapper'))
+            this.controlPanel = new GameControlPanel(gameHtmlWrapper.querySelector('#gameControlWrapper'), this.boardController.addMemberToTeam)
         }
 
     }
@@ -25,28 +25,44 @@ class BoardController {
         this.boardWrapper = boardHtmlWrapper;
         this.teamWrapperA = this.boardWrapper.querySelector('#team-wrapper-a');
         this.teamWrapperB = this.boardWrapper.querySelector('#team-wrapper-b');
+        this.teamA = [];
+        this.teamB = [];
+    }
+
+    addMemberToTeam = (member, team) => {
+        team === 0 ? this.teamA.push(member) : this.teamB.push(member);
+        console.log('thisTeams', this.teamA, this.teamB)
     }
 
 }
 
 class GameControlPanel {
-    constructor(gamePanelHtml) {
+    constructor(gamePanelHtml, addToTeamCallback) {
         this.gamePanel = gamePanelHtml;
 
         this.nameInput = this.gamePanel.querySelector('#name');
-        this.strengthInput = this.gamePanel.querySelector('#strength')
-        this.weaponInput = this.gamePanel.querySelector('#weapon')
-        this.teamSelect = this.gamePanel.querySelector('#default_select')
+        this.strengthInput = this.gamePanel.querySelector('#strength');
+        this.weaponInput = this.gamePanel.querySelector('#weapon');
+        this.teamSelect = this.gamePanel.querySelector('#default_select');
 
 
         this.randomPersonButton = this.gamePanel.querySelector('#randomPerson');
-        this.startBattleButton = this.gamePanel.querySelector('#startBattle')
+        this.startBattleButton = this.gamePanel.querySelector('#startBattle');
+        this.addToTeamButton = this.gamePanel.querySelector('#addToTeam');
+
+        this.newMember = null
 
         this.randomPersonButton.addEventListener('click', () => {
-            const newMember = this.drawPerson();
-            this.setInputs(newMember);
-        })
+            this.newMember = this.drawPerson();
+            this.setInputs(this.newMember);
 
+        })
+        this.addToTeamButton.addEventListener('click', () => {
+            if (this.newMember !== null) {
+                addToTeamCallback(this.newMember, this.teamSelect.value);
+            }
+        })
+        
     }
 
     drawPerson() {

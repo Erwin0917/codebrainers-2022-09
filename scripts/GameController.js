7935d8b2bcab60a276c77bd6f8ea8e3c4390c027
Ewelina Kociolek <ewelina.kociolek@wp.pl>
2023-01-31 21:04:50 +0100

    ek-exercise-06 fetch data from API

--- scripts/GameController.js
+++ scripts/GameController.js
@@ -2,22 +2,23 @@ import { Criminal, Hero, Character } from "./Character.js";
 import { between } from "./main.js";
 import { weaponDraw } from "./Weapon.js";
 
+const duel = (attacker, victim) => {
+    do {
+        if (attacker.isAlive() && victim.isAlive()) {
+            attacker.attack(victim);
+        }
+        if (victim.isAlive() && attacker.isAlive()) {
+            victim.attack(attacker);
+        }
+    } while (victim.isAlive() && attacker.isAlive());
+    };
+
 const isMemberDead = (member) => member.isAlive() === false;
 const isTeamDead = (team) => team.every(isMemberDead);
-const duel = (attacker, victim) => {
-  do {
-    if (attacker.isAlive() && victim.isAlive()) {
-      attacker.attack(victim);
-    }
-    if (victim.isAlive() && attacker.isAlive()) {
-      victim.attack(attacker);
-    }
-  } while (victim.isAlive() && attacker.isAlive());
-};
 
 export class GameController {
     constructor(gameHtmlWrapper) {
-        this.gameWrapper = gameHtmlWrapper;
+    this.gameWrapper = gameHtmlWrapper;
 
     if (gameHtmlWrapper.querySelector("#gameBoard")) {
         this.boardController = new BoardController(
@@ -26,70 +27,67 @@ export class GameController {
     }
 
     if (gameHtmlWrapper.querySelector("#gameControlWrapper")) {
-        this.controlPanel = new GameControlPanel(
-        gameHtmlWrapper.querySelector("#gameControlWrapper"),
-        this.boardController.addMemberToTeam
-        );
-        this.controlPanel.startBattleButton.addEventListener('click', this.battle)
-        }
+        this.controlPanel = new GameControlPanel(gameHtmlWrapper.querySelector("#gameControlWrapper"),this.boardController.addMemberToTeam);
+        this.controlPanel.startBattleButton.addEventListener('click',this.battle)
     }
-
-battle = () => {
-  do {
-    this.boardController.teamA.forEach((heroInBattle, index) => {
-      let criminalInBattle = this.boardController.teamB[index];
-      if (criminalInBattle.isAlive() === false) {
-        criminalInBattle = this.boardController.teamB.find((person) => person.isAlive());
-      }
-      if (criminalInBattle !== undefined) {
-        const opponents = [heroInBattle, criminalInBattle].sort(
-          () => 0.5 - Math.random()
-        );
-        duel(opponents[0], opponents[1]);
-      }
-    });
-  } while ((isTeamDead(this.boardController.teamA) || isTeamDead(this.boardController.teamB)) === false);
-  console.log(this.boardController.teamA);
-  console.log(this.boardController.teamB);
-};
+    }
+    battle = () => {
+    do {
+        this.boardController.teamA.forEach((heroInBattle, index) => {
+        let criminalInBattle = this.boardController.teamB[index];
+        if (criminalInBattle.isAlive() === false) {
+            criminalInBattle = this.boardController.teamB.find((person) => person.isAlive());
+        }
+        if (criminalInBattle !== undefined) {
+            const opponents = [heroInBattle, criminalInBattle].sort(
+            () => 0.5 - Math.random()
+            );
+            duel(opponents[0], opponents[1]);
+        }
+        });
+    } while ((isTeamDead(this.boardController.teamA) || isTeamDead(this.boardController.teamB)) === false);
+    console.log(this.boardController.teamA);
+    console.log(this.boardController.teamB);
+    };
 }
 
 class BoardController {
     constructor(boardHtmlWrapper) {
-        this.boardWrapper = boardHtmlWrapper;
-        this.teamWrapperA = this.boardWrapper.querySelector("#team-wrapper-a");
-        this.teamWrapperB = this.boardWrapper.querySelector("#team-wrapper-b");
-        this.teamA = [];
-        this.teamB = [];
+    this.boardWrapper = boardHtmlWrapper;
+    this.teamWrapperA = this.boardWrapper.querySelector("#team-wrapper-a");
+    this.teamWrapperB = this.boardWrapper.querySelector("#team-wrapper-b");
+    this.teamA = [];
+    this.teamB = [];
     }
 
     createPersonCard = (member) => {
+        console.log('member: ', member)
         if (member instanceof Character === false) {
         console.error("member not instance of Character");
         return;
-        }
+    }
 
     const personWrapper = document.createElement("div");
     personWrapper.classList.add("personWrapper");
     personWrapper.innerHTML = `
-        <div class='avatar'></div>
-        <div class='personName'>Name: ${member.name}</div>
-        <div class='personWeapon'>Weapon: ${member.weapon.name}</div>
-        <div class='personStrength'>Strength: ${member.strength}</div>
-        <div class='personHitPoints'>Hit Points: ${member.hitPoints}</div>
-        <progress class='nes-progress is-error' value='${member.hitPoints}' max='${member.hitPoints}'></progress>
-        `;
-        return personWrapper;
+    <div class='avatar'><img src='${member.imageUrl}'></div>
+    <div class='personName'>Name: ${member.name}</div>
+    <div class='personWeapon'>Weapon: ${member.weapon.name}</div>
+    <div class='personStrength'>Strength: ${member.strength}</div>
+    <div class='personHitPoints'>Hit Points: ${member.hitPoints}</div>
+    <progress class='nes-progress is-error' value='${member.hitPoints}' max='${member.hitPoints}'></progress>
+    `;
+    return personWrapper;
     };
 
     addMemberToTeam = (member, team) => {
         const personCard = this.createPersonCard(member);
         if (team === "0") {
-        this.teamA.push(member);
-        this.teamWrapperA.appendChild(personCard);
-    } else {
-        this.teamB.push(member);
-        this.teamWrapperB.appendChild(personCard);
+            this.teamA.push(member);
+            this.teamWrapperA.appendChild(personCard);
+        } else {
+            this.teamB.push(member);
+            this.teamWrapperB.appendChild(personCard);
         }
     };
 }
@@ -109,40 +107,45 @@ class GameControlPanel {
 
         this.newMember = null;
 
-    this.randomPersonButton.addEventListener(
+        this.randomPersonButton.addEventListener(
         "click",
         this.randomPersonAndSetInputs
     );
 
     this.addToTeamButton.addEventListener("click", () => {
         if (this.newMember !== null) {
-            addToTeamCallback(this.newMember, this.teamSelect.value); /// !!!!!!
+        addToTeamCallback(this.newMember, this.teamSelect.value); /// !!!!!!
         }
         this.randomPersonAndSetInputs();
         });
     }
 
-    randomPersonAndSetInputs = () => {
-    this.newMember = this.drawPerson();
+randomPersonAndSetInputs = async () => {
+    this.newMember = await this.drawPerson();
     this.setInputs(this.newMember);
     };
 
-    drawPerson() {
+    async drawPerson() {
+        const characterId = between(1,826)
+        const response = await fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
+        const characterData = await response.json()
         const personClass = Math.random() > 0.5 ? Hero : Criminal;
         const minMaxHP = {
         HP: between(120, 250),
         strength: between(50, 80),
-    };
-    const member = new personClass(minMaxHP.HP, minMaxHP.strength);
-    const weapon = weaponDraw();
-    member.setWeapon(weapon);
-    return member;
+        };
+        const member = new personClass(minMaxHP.HP, minMaxHP.strength);
+        member.name = characterData.name
+        member.imageUrl = characterData.image
+        const weapon = weaponDraw();
+        member.setWeapon(weapon);
+        return member;
     }
 
     setInputs = (member) => {
-    this.nameInput.value = member.name;
-    this.strengthInput.value = member.strength;
-    this.weaponInput.value = member.weapon.name;
-    this.teamSelect.value = Math.random() > 0.5 ? 0 : 1;
+        this.nameInput.value = member.name;
+        this.strengthInput.value = member.strength;
+        this.weaponInput.value = member.weapon.name;
+        this.teamSelect.value = Math.random() > 0.5 ? 0 : 1;
     };
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,24 @@
 import { GameController } from './GameController.js';
 
+const testPromise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('Finish test promise')
+    }, 5000);
+})
+
+// testPromise.them((test) => {
+//     console.log('test', test);
+// })
+
 export function between(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-(function () {
+(async function () {
   console.log('run');
   const gameHtmlWrapper = document.querySelector('#game-wrapper-one');
   const gameController = new GameController(gameHtmlWrapper);
   console.log('gameController ', gameController);
-
+  const test = await testPromise;
+  console.log('test', test);
 })();
